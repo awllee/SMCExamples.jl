de295f5fd4ea8eb6aa2e3da5fc05e0abb8b4895f
Waiss Azizian <waiss.azizian@ens.fr>
2021-12-15 21:00:48 +0100

    Remove tests

--- test/sv_test.jl
+++ test/sv_test.jl
@@ -11,14 +11,8 @@ n = 10
 nt = Threads.nthreads()
 model, guidedmodel, theta, ys = defaultSVModel(n)
 
-smcio = SMCIO{model.particle, model.pScratch}(N, n, nt, true)
+smcio = SMCIO{model.particle, model.pScratch}(N, n, nt, false)
 smc!(model, smcio)
 
-guidedsmcio = SMCIO{guidedmodel.particle, guidedmodel.pScratch}(N, n, nt, true)
+guidedsmcio = SMCIO{guidedmodel.particle, guidedmodel.pScratch}(N, n, nt, false)
 smc!(guidedmodel, guidedsmcio)
-
-f = p -> p.x
-@test V(smcio, f, false, false, n) ≥ V(guidedsmcio, f, false, false, n)
-@test V(smcio, f, false, true, n) ≥ V(guidedsmcio, f, false, true, n)
-@test V(smcio, f, true, false, n) ≥ V(guidedsmcio, f, true, false, n)
-@test V(smcio, f, true, true, n) ≥ V(guidedsmcio, f, true, true, n)
