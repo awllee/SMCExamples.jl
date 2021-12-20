66d5dcb4f5cd6d9f5335ae02fa3413507eb61f6c
Waiss Azizian <waiss.azizian@ens.fr>
2021-12-20 11:58:26 +0100

    Fix error in guided SMC and add example

--- src/stochVolatility.jl
+++ src/stochVolatility.jl
@@ -14,14 +14,20 @@ struct SVTheta
     ρ::Float64
 end
 
-function makeSVModel(theta::SVTheta, ys::Vector{Float64}; guide=false)
+mutable struct GuidedSVParticle
+  x::Float64
+  xprev::Float64
+  GuidedSVParticle() = new()
+end
+
+function makeSVModel(theta::SVTheta, ys::Vector{Float64})
     n::Int64 = length(ys)
     log2pi = -0.5 * log(2 * π)
     @inline function lG(p::Int64, particle::Float64Particle, ::Nothing)
         @inbounds v::Float64 = particle.x + ys[p]^2 / exp(particle.x)
         return log2pi - 0.5 * v
     end
-    @inline function naiveM!(newParticle::Float64Particle, rng::RNG, p::Int64,
+    @inline function M!(newParticle::Float64Particle, rng::RNG, p::Int64,
         particle::Float64Particle, ::Nothing)
         if p == 1
             newParticle.x = theta.μ + theta.σ*randn(rng)
@@ -29,22 +35,45 @@ function makeSVModel(theta::SVTheta, ys::Vector{Float64}; guide=false)
             newParticle.x = theta.μ + theta.ρ*(particle.x - theta.μ) + theta.σ*randn(rng)
         end
     end
-    @inline function guideM!(newParticle::Float64Particle, rng::RNG, p::Int64,
-        particle::Float64Particle, ::Nothing)
-        μstar::Float64 = if p == 1
-            theta.μ 
-        else
-            theta.μ + theta.ρ*(particle.x - theta.μ)
-        end
-        @inbounds ξ::Float64 = μstar + 0.25*theta.σ^2*(ys[p]^2*exp(-μstar) - 2)
-        newParticle.x = ξ + theta.σ*randn(rng)
-    end
-    M! = guide ? guideM! : naiveM!
     return SMCModel(M!, lG, n, Float64Particle, Nothing)
 end
 
+function makeguidedSVModel(theta::SVTheta, ys::Vector{Float64}; guide=false)
+    n::Int64 = length(ys)
+    log2pi = -0.5 * log(2 * π)
+    @inline function computeξ(p::Int64, x::Float64)
+        μstar::Float64 = if p == 1
+            theta.μ 
+        else
+            theta.μ + theta.ρ*(x - theta.μ)
+        end
+        @inbounds ξ::Float64 = μstar + 0.25*theta.σ^2*(ys[p]^2*exp(-μstar) - 2)
+        return ξ
+    end
+    @inline function lG(p::Int64, particle::GuidedSVParticle, ::Nothing)
+        @inbounds y::Float64 = ys[p]
+        loggp::Float64 = -0.5*(particle.x + y^2/exp(particle.x))
+        v::Float64 = if p == 1
+            theta.μ
+        else
+            theta.μ + theta.ρ*(particle.xprev - theta.μ)
+        end
+        logfp::Float64 = -0.5*(particle.x - v)^2/theta.σ^2
+        ξ::Float64 = computeξ(p, particle.xprev)
+        logqp::Float64 = -0.5*(particle.x - ξ)^2/theta.σ^2
+        return logfp + loggp - logqp
+    end
+    @inline function M!(newParticle::GuidedSVParticle, rng::RNG, p::Int64,
+        particle::GuidedSVParticle, ::Nothing)
+        newParticle.xprev = particle.x
+        ξ::Float64 = computeξ(p, particle.x)
+        newParticle.x = ξ + theta.σ*randn(rng)
+    end
+    return SMCModel(M!, lG, n, GuidedSVParticle, Nothing)
+end
+
 function simulateSVModel(theta::SVTheta, n::Int64)
-  model = makeSVModel(theta, Vector{Float64}(undef, 0); guide=false)
+  model = makeSVModel(theta, Vector{Float64}(undef, 0))
   ys = Vector{Float64}(undef, n)
   xParticle = Float64Particle()
   rng = getRNG()
@@ -59,8 +88,8 @@ function defaultSVModel(n::Int64)
     theta = SVTheta(0.2, -1, 0.97)
     ys = simulateSVModel(theta, n)
 
-    svModel = makeSVModel(theta, ys; guide=false)
-    guidedSvModel = makeSVModel(theta, ys; guide=true)
+    svModel = makeSVModel(theta, ys)
+    guidedSvModel = makeguidedSVModel(theta, ys)
 
     return svModel, guidedSvModel, theta, ys
 end
--- test/sv_test.jl
+++ test/sv_test.jl
@@ -1,18 +1,32 @@
 using SequentialMonteCarlo
-import SequentialMonteCarlo.V
+import SequentialMonteCarlo: V, eta
 using RNGPool
 import SMCExamples.StochVolatility.defaultSVModel
 using Test
+import Statistics: std, mean
 
 setRNGs(0)
 
 N = 32768
 n = 10
+numTrials = 5
 nt = Threads.nthreads()
 model, guidedmodel, theta, ys = defaultSVModel(n)
 
-smcio = SMCIO{model.particle, model.pScratch}(N, n, nt, false)
-smc!(model, smcio)
+function estimate_mean(; guided=false, filtering=false)
+    m = guided ? guidedmodel : model
+    smcio = SMCIO{m.particle, m.pScratch}(N, n, nt, false)
+    smc!(m, smcio)
+    return eta(smcio, particle -> particle.x, filtering, n)
+end
 
-guidedsmcio = SMCIO{guidedmodel.particle, guidedmodel.pScratch}(N, n, nt, false)
-smc!(guidedmodel, guidedsmcio)
+guided_predictive_means = [estimate_mean(guided=true, filtering=false) for _ in 1:numTrials]
+guided_filtering_means  = [estimate_mean(guided=true, filtering=true) for _ in 1:numTrials]
+predictive_means        = [estimate_mean(guided=false, filtering=false) for _ in 1:numTrials]
+filtering_means         = [estimate_mean(guided=false, filtering=true) for _ in 1:numTrials]
+
+@test mean(guided_filtering_means) ≈ mean(filtering_means) atol=0.05
+@test std(guided_filtering_means) ≤ std(filtering_means)
+
+@test mean(guided_predictive_means) ≈ mean(predictive_means) atol=0.05
+
